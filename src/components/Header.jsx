4c8f0ed83d661c638abed06e1810c5255dc4f873
Maided <maided.h.p90@gmail.com>
2021-04-30 23:08:29 -0500

    añadiendo más cosas, jsjs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import '../assets/styles/Header.scss'
 const Header = () => {
   return (
-    <div>Hola mundo</div>
+    <div className='container'>
+      <div className='lbl-menu'>
+        <label htmlFor='radio'>Inicio</label>
+        <label htmlFor='radio2'>Quién soy</label>
+        <label htmlFor='radio3'>Mi proyecto</label>
+      </div>
+
+      <div className='content'>
+
+        <input type='radio' name='radio' id='radio' />
+        <div className='tab1'>
+          <h2>Bienvenidos!</h2>
+        </div>
+
+        <input type='radio' name='radio' id='radio2' />
+        <div className='tab2'>
+          <h2>Soy Maided!</h2>
+        </div>
+
+        <input type='radio' name='radio' id='radio3' />
+        <div className='tab3'>
+          <h2>Este proyecto...</h2>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../assets/styles/App.scss'
 import Header from '../components/Header.jsx'
 const Home = () => {
   return (
